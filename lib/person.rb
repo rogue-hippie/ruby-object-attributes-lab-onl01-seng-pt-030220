b34de6cfd4ed8277e4f8c28ed13e4f3e5aa1ed43
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2020-03-24 20:22:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -12,6 +12,17 @@ class Person
     @name = new_name
   end
   
+  def initialize
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
   
 
 end
