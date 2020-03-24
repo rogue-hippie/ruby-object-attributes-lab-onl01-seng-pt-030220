a5f4acb50390b5546644b2881c3959405e5d9361
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2020-03-24 20:19:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,6 +13,15 @@ class Dog
   end
     
   def initialize
-    @breed 
+    @breed = breed 
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
   
 end 
